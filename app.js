b7115c554b50d280c0221145de00de4b0a74997f
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-02-24 00:28:39 +0530

    add route functionality

---
+++ app.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const constants = require('./utils/constants');
+
+// Routes
+const hotelRoutes = require('./routes/hotel');
+const adminRoutes = require('./routes/admin');
+const authRoutes = require('./routes/auth');
+
+// Controllers
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', (req, res, next) => {
+//     res.render('hotel/home');
+// });
+
+app.use('/admin', adminRoutes);
+app.use(hotelRoutes);
+app.use(authRoutes);
+app.use(errorController.get404);
+
+app.listen(constants.PORT);
