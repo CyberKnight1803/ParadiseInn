32bcf9e305a86cc9faad554a2b16afff0e1cac6a
CyberKnight1803 <omkarpitale2001@gmail.com>
2022-02-24 00:28:07 +0530

    views init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./views/**/*.{ejs, js, html}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: "'Roboto Mono'",
+        dm_mono: "'DM Mono'", 
+        archiabold: "'archia-bold'",
+        archia: "'archia'",
+        archiasemibold: "'archia-semibold'",
+      }
+    },
+  },
+  plugins: [],
+};
